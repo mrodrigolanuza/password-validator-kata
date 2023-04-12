9af9bd5b5958f04a19de568ecbdfba799d08df7f
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-12 18:23:23 +0200

    Preparación del módulo de test y lista de ejemplos TDD

---
+++ src/tests/passwordValidator.test.ts
@@ -0,0 +1,15 @@
+/*
+Lista de ejemplos:
+
+1234abcdABCD_ > OK
+1aA_ - No tiene longitud suficiente
+abcdABCD_ - No tiene números
+1234ABCD_ - No tiene minúsculas
+1234abcd_ No tiene mayúsculas
+1234abcdABCD No tiene guión bajo
+
+*/
+
+describe("The Password validator should..", ()=>{
+    
+});
