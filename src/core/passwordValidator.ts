d6696ca7a6da0f7909e48483e8e61efdc2b5e46f
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-12 18:57:43 +0200

    Green: La contraseña debe contener números

--- src/core/passwordValidator.ts
+++ src/core/passwordValidator.ts
@@ -1,5 +1,9 @@
 export function isStrongPassword(password:string):boolean{
     if(password.length < 6)
         return false;
+    
+    const regex = /\d/;
+    if(!regex.test(password))
+        return false;
     return true;
 }
